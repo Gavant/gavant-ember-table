f9112943bed209d76273128f24c15499d7eb5e91
npafundi <npafundi@gavant.com>
2020-01-28 15:16:34 -0500

    further fa-icon work

--- addon/components/fa-icon/fa-icon.ts
+++ addon/components/fa-icon/fa-icon.ts
@@ -1,3 +1,7 @@
 import FaIcon from 'ember-font-awesome/addon/components/fa-icon/component';
+// @ts-ignore: Ignore import of compiled template
+import layout from '@gavant/ember-table/templates/components/fa-icon/fa-icon';
 
-export default class FaIconComponent extends FaIcon {}
+export default class FaIconComponent extends FaIcon {
+    layout = layout;
+}
