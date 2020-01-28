8f2549dc7f255bf80bb8cb1e7ade9ee31fff63da
npafundi <npafundi@gavant.com>
2020-01-28 15:52:56 -0500

    changed ember-font-awesome to the correct version

--- addon/components/fa-icon/fa-icon.ts
+++
@@ -1,7 +0,0 @@
-import FaIcon from 'ember-font-awesome/components/fa-icon/component';
-// @ts-ignore: Ignore import of compiled template
-import layout from '@gavant/ember-table/templates/components/fa-icon/fa-icon';
-
-export default class FaIconComponent extends FaIcon {
-    layout = layout;
-}
