fb22de94154b170a38f46f54ec93ba4d23529966
npafundi <npafundi@gavant.com>
2020-01-29 10:49:17 -0500

    testing renaming of components to fix issue where apps are using the ember-table components instead

--- addon/components/ember-tbody/ember-tbody.ts
+++ addon/components/ember-tbody/ember-tbody.ts
@@ -1,7 +1,7 @@
-import EmberTbody from 'ember-table/components/ember-tbody/component';
+import EmberTbodyComponent from 'ember-table/components/ember-tbody/component';
 // @ts-ignore: Ignore import of compiled template
 import layout from '@gavant/ember-table/templates/components/ember-tbody/ember-tbody';
 
-export default class EmberTbodyComponent extends EmberTbody {
+export default class EmberTbody extends EmberTbodyComponent {
     layout = layout;
 }
