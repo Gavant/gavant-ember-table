2400ee4eaf9b5fbd260485d5ac52588e7f915993
npafundi <npafundi@gavant.com>
2020-01-29 11:07:28 -0500

    reverted component names, solved import issue by adding 'after': ['ember-table'] to the addon's pack.json under 'ember-addon'

--- addon/components/ember-table/ember-table.ts
+++ addon/components/ember-table/ember-table.ts
@@ -1,7 +1,7 @@
-import EmberTableComponent from 'ember-table/components/ember-table/component';
+import EmberTable from 'ember-table/components/ember-table/component';
 // @ts-ignore: Ignore import of compiled template
 import layout from '@gavant/ember-table/templates/components/ember-table/ember-table';
 
-export default class EmberTable extends EmberTableComponent {
+export default class EmberTableComponent extends EmberTable {
     layout = layout;
 }
