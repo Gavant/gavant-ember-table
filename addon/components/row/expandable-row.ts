5a8aea215aff7887a7e3b54e70e926bdb384b4cd
Nick Pafundi <npafundi@gavant.com>
2020-07-24 15:21:52 -0400

    second TR style update

--- addon/components/row/expandable-row.ts
+++ addon/components/row/expandable-row.ts
@@ -1,7 +1,12 @@
 import TableRow from '../row/table-row';
+import Ember from 'ember';
 
 export default class ExpandableRow extends TableRow {
     get isExpanded() {
         return this.args.tableMeta.expandedRows?.includes(this.rowValue) ?? false;
+    }
+
+    get collapsedStyle() {
+        return Ember.String.htmlSafe(!this.isExpanded ? 'display:none;' : '');
     }
 }
