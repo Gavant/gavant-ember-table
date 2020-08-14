4265f11ff934611869ba633053c7bf05271b38a9
Bill Dami <bdami@gavant.com>
2020-08-14 07:38:50 -0400

    fix missing module declarations for copied js files

--- types/ember-table/index.d.ts
+++ types/ember-table/index.d.ts
@@ -1,5 +1,7 @@
 declare module 'ember-table/components/ember-table/component';
 declare module 'ember-table/components/ember-tbody/component';
+declare module '@gavant/ember-table/-private/sticky/legacy-sticky-polyfill';
+declare module '@gavant/ember-table/-private/sticky/table-sticky-polyfill';
 
 declare module '@gavant/ember-table' {
     export interface ColumnValue {
