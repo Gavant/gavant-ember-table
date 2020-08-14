1a9bcb6f166bccc7599c821d29848de8d5b842a1
Adam Baker <abaker@gavant.com>
2020-08-14 08:13:13 -0400

    Adding polyfills back in

--- types/ember-table/index.d.ts
+++ types/ember-table/index.d.ts
@@ -1,2 +1,4 @@
 declare module 'ember-table/components/ember-table/component';
 declare module 'ember-table/components/ember-tbody/component';
+declare module '@gavant/ember-table/-private/sticky/legacy-sticky-polyfill';
+declare module '@gavant/ember-table/-private/sticky/table-sticky-polyfill';
