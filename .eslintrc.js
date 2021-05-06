4d6ace9e2899758a640469cb4bf40456ca1b53ba
Adam Baker <bakerac4@gmail.com>
2021-05-06 09:12:19 -0400

    Upgrading to typescript lint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,41 @@
 module.exports = {
     root: true,
-    parser: 'babel-eslint',
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module'
-    },
-    plugins: ['ember', 'prettier'],
-    extends: ['eslint:recommended', 'plugin:ember/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['ember', '@typescript-eslint', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:ember/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'plugin:prettier/recommended'
+    ],
     env: {
         browser: true
     },
     rules: {
-        //@see http://eslint.org/docs/rules/no-var
-        'no-var': 'error',
-        //@see http://eslint.org/docs/rules/object-shorthand
-        'object-shorthand': 'error',
-        //@see http://eslint.org/docs/rules/prefer-template
-        'prefer-template': 'error',
-        'prettier/prettier': 'error'
+        'ember/no-jquery': 'error',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-empty-interface': 'off'
     },
     overrides: [
         // node files
         {
             files: [
+                '.eslintrc.js',
+                '.template-lintrc.js',
                 'ember-cli-build.js',
                 'index.js',
                 'testem.js',
                 'blueprints/*/index.js',
                 'config/**/*.js',
-                'tests/dummy/config/**/*.js',
-                '.eslintrc.js'
+                'tests/dummy/config/**/*.js'
             ],
             excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
             parserOptions: {
-                sourceType: 'script',
-                ecmaVersion: 2015
+                sourceType: 'script'
             },
             env: {
                 browser: false,
@@ -43,10 +44,6 @@ module.exports = {
             plugins: ['node'],
             rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
                 // add your custom rules and overrides for node files here
-
-                // this can be removed once the following is fixed
-                // https://github.com/mysticatea/eslint-plugin-node/issues/77
-                'node/no-unpublished-require': 'off'
             })
         }
     ]
