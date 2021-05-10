8c1cc69e94051f421ae6283d77c36d3f414799ac
Adam Baker <bakerac4@gmail.com>
2021-05-10 12:59:45 -0400

    Fixing tests

--- addon/components/row/no-results.ts
+++ addon/components/row/no-results.ts
@@ -1,12 +1,9 @@
 import Component from '@glimmer/component';
 
-import layout from '@gavant/ember-table/templates/components/row/no-results';
-
 interface RowNoResultsArgs {
     columns: any[];
 }
 export default class RowNoResultsComponent extends Component<RowNoResultsArgs> {
-    layout = layout;
     tagName: string = 'tr';
     classNames: string[] = ['et-tr', 'data-table-row-no-results'];
     cellClassNames: string = 'p-4 text-muted';
