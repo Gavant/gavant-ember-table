83e9d1ff302a9fbe9458e15302c90e134afea814
Adam Baker <bakerac4@gmail.com>
2022-07-12 14:45:25 -0400

    Update and fix lint

--- ember-table/src/components/progress-spinner/index.ts
+++ ember-table/src/components/progress-spinner/index.ts
@@ -1,9 +1,22 @@
 import Component from '@glimmer/component';
 
-export default class ProgressSpinnerComponent extends Component {
-    classNames: string[] = ['progress-spinner'];
+interface ProgressSpinnerArgs {
+    active?: boolean;
+    light?: boolean;
+    size?: 'small' | 'medium' | 'large';
+}
+export default class ProgressSpinnerComponent extends Component<ProgressSpinnerArgs> {
     classNameBindings: string[] = ['active', 'light', 'size'];
-    active: boolean = true;
-    light: boolean = false;
-    size: string | null = null;
+
+    get active() {
+        return this.args.active ?? true;
+    }
+
+    get light() {
+        return this.args.light ?? false;
+    }
+
+    get size() {
+        return this.args.size ?? null;
+    }
 }
